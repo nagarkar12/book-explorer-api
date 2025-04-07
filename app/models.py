ab7fcf6de6ab43df9a32abe28b4609881288a831
ShrutiLanjekar <shrutilanjekar13@gmail.com>
2025-04-07 17:44:18 +0530

    Initial commit

---
+++ app/models.py
@@ -0,0 +1,8 @@
+from . import db
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    author = db.Column(db.String(100))
+    genre = db.Column(db.String(50))
+    year = db.Column(db.Integer)
---
+++ app/routes.py
@@ -0,0 +1,27 @@
+from flask import request, jsonify
+from .models import Book
+from . import db
+from flask import current_app as app
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Welcome to Book Explorer API!"})
+
+@app.route('/books', methods=['POST'])
+def add_book():
+    data = request.get_json()
+    new_book = Book(**data)
+    db.session.add(new_book)
+    db.session.commit()
+    return jsonify({"message": "Book added!"}), 201
+
+@app.route('/books', methods=['GET'])
+def get_books():
+    books = Book.query.all()
+    return jsonify([{
+        "id": b.id,
+        "title": b.title,
+        "author": b.author,
+        "genre": b.genre,
+        "year": b.year
+    } for b in books])
