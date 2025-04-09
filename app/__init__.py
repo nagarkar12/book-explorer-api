1fc48f287912ee3100c0411a43475cad01ad0690
ShrutiLanjekar <shrutilanjekar13@gmail.com>
2025-04-09 14:43:50 +0530

    Initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 db = SQLAlchemy()
 
@@ -7,12 +8,11 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../instance/book.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SECRET_KEY'] = 'your-secret-key'  # Required for JWT
 
     db.init_app(app)
 
     with app.app_context():
-        from . import routes  # Register your routes
+        from . import routes
         db.create_all()
 
     return app
--- app/models.py
+++ app/models.py
@@ -1,12 +1,8 @@
-from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
-class User(db.Model):
+class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
-    password_hash = db.Column(db.String(200), nullable=False)
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-    
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+    title = db.Column(db.String(100))
+    author = db.Column(db.String(100))
+    genre = db.Column(db.String(50))
+    year = db.Column(db.Integer)
