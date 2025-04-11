9da27bccb8eb32ec0bd8040712a1e4c5d81a9ab0
ShrutiLanjekar <shrutilanjekar13@gmail.com>
2025-04-11 17:00:06 +0530

    project

--- app/routes.py
+++ app/routes.py
@@ -1,30 +1,28 @@
 from flask import request, jsonify
 from .models import Book
 from . import db
-from flask import current_app as app
 
-@app.route('/')
-def home():
-    return jsonify({"message": "Welcome to Book Explorer API!"})
+def register_routes(app):
+    @app.route('/')
+    def home():
+        return jsonify({"message": "Welcome to Book Explorer API!"})
 
-@app.route('/books', methods=['POST'])
-def add_book():
-    data = request.get_json()
-    new_book = Book(**data)
-    db.session.add(new_book)
-    db.session.commit()
-    return jsonify({"message": "Book added!"}), 201
+    @app.route('/books', methods=['POST'])
+    def add_book():
+        data = request.get_json()
+        new_book = Book(**data)
+        db.session.add(new_book)
+        db.session.commit()
+        return jsonify({"message": "Book added!"}), 201
 
-@app.route("/books", methods=["GET"])
-def get_books():
-    books = Book.query.all()
-    return jsonify([{
-        "id": b.id,
-        "title": b.title,
-        "author": b.author,
-        "year": b.year,
-        "description": b.description,
-        "genre": b.genre
-    } for b in books])
-
-
+    @app.route("/books", methods=["GET"])
+    def get_books():
+        books = Book.query.all()
+        return jsonify([{
+            "id": b.id,
+            "title": b.title,
+            "author": b.author,
+            "year": b.year,
+            "description": b.description,
+            "genre": b.genre
+        } for b in books])
