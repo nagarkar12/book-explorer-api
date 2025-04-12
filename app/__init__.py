b6d2bd4ae1607e4da9fcaf0124e217130aba6fcf
ShrutiLanjekar <shrutilanjekar13@gmail.com>
2025-04-12 08:38:16 +0530

    Add seeded books to book.db

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     basedir = os.path.abspath(os.path.dirname(__file__))
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../instance/book.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/book.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
