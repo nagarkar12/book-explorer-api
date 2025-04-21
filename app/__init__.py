5ee02c0eafca23b6b1767125b41912b58d636553
ShrutiLanjekar <shrutilanjekar13@gmail.com>
2025-04-21 11:35:14 +0530

    fix: added full CORS support

--- app/__init__.py
+++ app/__init__.py
@@ -7,16 +7,13 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../instance/book.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
-    CORS(app) 
-    
+    CORS(app, resources={r"/*": {"origins": "*"}})
+
     from .routes import book_routes
     app.register_blueprint(book_routes)
-
-    with app.app_context():
-        db.create_all()
 
     return app
