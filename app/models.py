6123440febad032e17555a7cd1915c26f7475628
ShrutiLanjekar <shrutilanjekar13@gmail.com>
2025-04-21 10:55:42 +0530

    project

--- app/models.py
+++ app/models.py
@@ -6,20 +6,20 @@ db = SQLAlchemy()
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(150), nullable=False)
     author = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.Text)  # Optional book summary
-    published_year = db.Column(db.Integer)  # Year of publication
-    genre = db.Column(db.String(50))  # Book genre like Fiction, History etc.
-    isbn = db.Column(db.String(20))  # Optional, International Standard Book Number
+    description = db.Column(db.Text)
+    published_year = db.Column(db.Integer)
+    genre = db.Column(db.String(50))
+    isbn = db.Column(db.String(20))
 
     def to_dict(self):
         return {
-            'id': self.id,
-            'title': self.title,
-            'author': self.author,
-            'description': self.description,
-            'published_year': self.published_year,
-            'genre': self.genre,
-            'isbn': self.isbn
+            "id": self.id,
+            "title": self.title,
+            "author": self.author,
+            "description": self.description,
+            "published_year": self.published_year,
+            "genre": self.genre,
+            "isbn": self.isbn
         }
